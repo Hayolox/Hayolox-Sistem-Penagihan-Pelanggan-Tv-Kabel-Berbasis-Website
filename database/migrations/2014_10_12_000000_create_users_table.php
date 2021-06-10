886a949322779ca3b3e4ee2c311000ed7750df06
Hayolox <akikli72@gmail.com>
2021-06-10 12:28:41 +0700

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->integer('Nomor_pelanggan');
             $table->string('name');
             $table->string('email')->unique();
+            $table->integer('no_hp');
+            $table->string('alamat');
+            $table->integer('tagihan');
+            $table->string('roles')->default('USERS');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- database/migrations/2021_06_10_050411_create_years_table.php
+++ database/migrations/2021_06_10_050411_create_years_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBillsTable extends Migration
+class CreateYearsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,9 @@ class CreateBillsTable extends Migration
      */
     public function up()
     {
-        Schema::create('bills', function (Blueprint $table) {
+        Schema::create('years', function (Blueprint $table) {
             $table->id();
+            $table->integer('tahun');
             $table->timestamps();
         });
     }
@@ -26,6 +27,6 @@ class CreateBillsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('bills');
+        Schema::dropIfExists('years');
     }
 }
