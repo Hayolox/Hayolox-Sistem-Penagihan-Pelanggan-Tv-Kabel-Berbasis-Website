af1e35b636ac8e22fc3e773e719393fdec35bfde
Hayolox <akikli72@gmail.com>
2021-06-14 09:32:16 +0700

    update

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Bill;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('pages.Admin.Dashboard');
+        $users = User::where('roles', 'USERS')->count();
+        $bills = Bill::count();
+        return view('pages.Admin.Dashboard',compact('users','bills'));
     }
 }
